5b00b0ffba4431716a0a3c85ce7e59caf9150529
DE-IGNIS <chauhansudarsh@gmail.com>
2026-02-23 14:34:26 +0530

    Configure CORS with wildcard origin and update default port to 5000

--- root/server/index.js
+++ root/server/index.js
@@ -4,11 +4,15 @@ const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
 require("dotenv").config();
 
-const PORT = process.env.PORT | "8000";
+const PORT = process.env.PORT || "5000";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
